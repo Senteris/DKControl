d552b9f3a2ced0fe1de503b8f61b7ad296d864c3
Xefise <Xefise@gmail.com>
2020-03-05 18:09:37 +0300

    Set urls and start making models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+#region Interfaces
+class IParent():
+
+class IStudent():
+
+#endregion
+
+#region abstract classes
+class User(AbstractUser):
+    patronymic = models.CharField('Отчество', max_length=32, blank=True, null=True)
+    birthday = models.DateField('Дата рождения')
+
+    GENDER_CHOISES = (
+        'Женский', 'Женский',
+        'Мужской', 'Мужской'
+    )
+    gender = models.CharField('Пол', max_length=7, choices=GENDER_CHOISES)
+
+
+#endregion
+
+#region models
+class Parent(IParent, User):
+
+
+class Student(IStudent, User):
+
+#endregion
