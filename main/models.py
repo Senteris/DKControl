5abab5891fd4ab1aaaca5631a15e2028314f49c5
Xefise <Xefise@gmail.com>
2020-03-06 19:57:32 +0300

    Interfaces -> Abstract classes

--- main/models.py
+++ main/models.py
@@ -1,17 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class Group(models.Model):
+    name = models.CharField("Название", max_length=16)
+    educator = models.ManyToManyField("Educator", "Преподаватель")
 
-#region Interfaces
-class IParent():
+#region Abstract
+class AParent():
 
-class IStudent():
+
+
+class AStudent():
+    school = models.CharField('Школа', max_length=32)
+    grade = models.CharField('Класс', max_length=3)
+    groups = models.ManyToManyField(Group, related_name="группы", null=True, blank=True)
+    parents = models.ManyToManyField("Parent", related_name="группы", null=True, blank=True)
+
+class AEducator():
 
 #endregion
 
 #region abstract classes
-class User(AbstractUser):
-    patronymic = models.CharField('Отчество', max_length=32, blank=True, null=True)
+class Person():
+    firstName = models.CharField("Имя", max_length=16)
+    lastName = models.CharField("Фамилия", max_length=16)
+    patronymic = models.CharField('Отчество', max_length=16, blank=True, null=True)
     birthday = models.DateField('Дата рождения')
 
     GENDER_CHOISES = (
@@ -20,13 +33,22 @@ class User(AbstractUser):
     )
     gender = models.CharField('Пол', max_length=7, choices=GENDER_CHOISES)
 
+    address = models.CharField('Адрес', max_length=128)
+    phone = models.IntegerField('Телефон', max_length=11)
 
+class User(AbstractUser, Person):
 #endregion
 
 #region models
-class Parent(IParent, User):
+class Parent(AParent, User):
+
+    def __str__(self):
+        return f"Родитель: {self.first_name} {self.last_name} {self.patronymic:1}."
 
 
-class Student(IStudent, User):
+class Student(AStudent, User):
+
+    def __str__(self):
+        return f"Ученик: {self.first_name} {self.last_name} {self.patronymic:1}. | Группа: {self.group} "
 
 #endregion
