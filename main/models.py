453f5deddad1f4a6b165d8e6eb2ce602873b6d36
Xefise <Xefise@gmail.com>
2020-03-07 16:30:47 +0300

    Make models

--- main/models.py
+++ main/models.py
@@ -1,54 +1,48 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class Group(models.Model):
     name = models.CharField("Название", max_length=16)
-    educator = models.ManyToManyField("Educator", "Преподаватель")
 
-#region Abstract
-class AParent():
-
-
-
-class AStudent():
-    school = models.CharField('Школа', max_length=32)
-    grade = models.CharField('Класс', max_length=3)
-    groups = models.ManyToManyField(Group, related_name="группы", null=True, blank=True)
-    parents = models.ManyToManyField("Parent", related_name="группы", null=True, blank=True)
-
-class AEducator():
-
-#endregion
-
-#region abstract classes
-class Person():
-    firstName = models.CharField("Имя", max_length=16)
-    lastName = models.CharField("Фамилия", max_length=16)
+class Profile(models.Model):
+    first_name = models.CharField("Имя", max_length=16)
+    last_name = models.CharField("Фамилия", max_length=16)
     patronymic = models.CharField('Отчество', max_length=16, blank=True, null=True)
     birthday = models.DateField('Дата рождения')
 
     GENDER_CHOISES = (
-        'Женский', 'Женский',
-        'Мужской', 'Мужской'
+        ('Женский', 'Женский'),
+        ('Мужской', 'Мужской')
     )
     gender = models.CharField('Пол', max_length=7, choices=GENDER_CHOISES)
 
     address = models.CharField('Адрес', max_length=128)
-    phone = models.IntegerField('Телефон', max_length=11)
+    phone = models.IntegerField('Телефон')
 
-class User(AbstractUser, Person):
-#endregion
+    class Meta:
+        abstract = True
+
+class User(AbstractUser, Profile):
+    #Ctrl+C Ctrl+V
+    first_name = models.CharField("Имя", max_length=16)
+    last_name = models.CharField("Фамилия", max_length=16)
+
+    groups = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name="Группы", null=True, blank=True)
 
 #region models
-class Parent(AParent, User):
+class Parent(Profile):
 
     def __str__(self):
         return f"Родитель: {self.first_name} {self.last_name} {self.patronymic:1}."
 
 
-class Student(AStudent, User):
+class Student(Profile):
+    school = models.CharField('Школа', max_length=32)
+    grade = models.CharField('Класс', max_length=3)
+    groups = models.ManyToManyField(Group, related_name="Группы", blank=True)
+    parents = models.ManyToManyField(Parent, related_name="Родители", blank=True)
 
     def __str__(self):
-        return f"Ученик: {self.first_name} {self.last_name} {self.patronymic:1}. | Группа: {self.group} "
-
+        return f"Ученик: {self.first_name} {self.last_name} {self.patronymic:1}. | Группа: {self.groups} "
 #endregion
