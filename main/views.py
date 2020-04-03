6114e66d85f809dbe68b39bd4aa447c463dfad19
Neisvestney <neisvestney113@gmail.com>
2020-04-03 22:15:58 +0300

    Add search system

--- main/views.py
+++ main/views.py
@@ -2,7 +2,11 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as user_login
 from django.contrib.auth import logout as user_logout
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+
+from main.models import Student, User, Parent, Union, Group, TimetableElem
 
 
 @login_required(login_url="login/")
@@ -33,4 +37,34 @@ def login(request):
 def logout(request):
     user_logout(request)
     return redirect('index')
+
+
+@login_required(login_url="login/")
+def search(request):
+    query = request.GET.get('q')
+    if query is None:
+        return render(request, 'index.html')
+    elif len(query) == 0:
+        return JsonResponse({})
+    else:
+        students = Student.objects.filter(
+            (Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(patronymic__icontains=query)) & Q(isDeleted=False)
+        )
+        teachers = User.objects.filter(
+            (Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(patronymic__icontains=query)) & Q(groups__name='Педагог')
+        )
+        parents = Parent.objects.filter(
+            Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(patronymic__icontains=query)
+        )
+        groups = Group.objects.filter(
+            Q(union__name=query) | Q(name__icontains=query)
+        )
+        timetable_elems = TimetableElem.objects.filter(
+            Q(beginTimeStr__icontains=query)
+        )
+        return JsonResponse({'students': [f"{s.first_name} {s.last_name} {s.patronymic}" for s in students],
+                             'parents': [f"{s.first_name} {s.last_name} {s.patronymic}" for s in parents],
+                             'teachers': [f"{s.first_name} {s.last_name} {s.patronymic}" for s in teachers],
+                             'groups': [f"{s.name} {s.union.name}" for s in groups],
+                             'timetable_elems': [f"{s.beginTimeStr}" for s in timetable_elems]})
 
