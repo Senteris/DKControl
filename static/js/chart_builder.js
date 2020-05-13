d2b16595c561c6956dd27e6b9ad3d3081a3ea849
Neisvestney <neisvestney113@gmail.com>
2020-05-13 14:21:20 +0300

    Add chart

--- static/js/chart_builder.js
+++ static/js/chart_builder.js
@@ -1,41 +1,47 @@
-$(document).ready(function(){
+function buildChart(id, url, label) {
     $.ajax({
-        url: "http://your_way",
+        url: url,
         method: "GET",
-        dataType : 'JSON',
-        success: function(data) {
+        dataType: 'JSON',
+        success: function (data) {
             console.log(data);
-            let region = [];
-            let value = [];
-
-            for (let i in data) {
-                region.push(data[i].CHART_NAME);
-                value.push(data[i].REQ_VALUE);
-            }
+            let region = data.region;
+            let value = data.value;
 
             var chartdata = {
                 labels: region,
-                datasets : [
+                datasets: [
                     {
-                        label: 'Region',
+                        label: label,
                         backgroundColor: 'rgba(200, 200, 200, 0.75)',
                         borderColor: 'rgba(200, 200, 200, 0.75)',
                         hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
                         hoverBorderColor: 'rgba(200, 200, 200, 1)',
-                        data: value.map(function(x) {return x * 1;})
+                        data: value.map(function (x) {
+                            return x * 1;
+                        })
                     }
                 ]
             };
 
-            var ctx = $("#chart_id");
+            var ctx = $("#" + id);
 
             var barGraph = new Chart(ctx, {
                 type: 'bar',
-                data: chartdata
+                data: chartdata,
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
             });
         },
-        error: function(data) {
+        error: function (data) {
             console.log(data);
         }
     });
-});
+}
