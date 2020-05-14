2a747cef641e687e871eef7fe22e4a70fe8f7045
Neisvestney <neisvestney113@gmail.com>
2020-05-14 11:15:56 +0300

    Fixed sidebar and icons

--- static/js/search.js
+++ static/js/search.js
@@ -1,23 +1,5 @@
 $(document).ready(function () {
-    let input = $("#search-off");
-    input.keyup(function () {
-        let result = $('#search-result');
-        if (this.value.length !== 0) {
-            search(this.value).then(function (data) {
-                let out = "";
-                for (let elem in data['students']) out += "<br>" + data['students'][elem];
-                for (let elem in data['teachers']) out += "<br>" + data['teachers'][elem];
-                for (let elem in data['parents']) out += "<br>" + data['parents'][elem];
-                for (let elem in data['groups']) out += "<br>" + data['groups'][elem];
-                for (let elem in data['timetable_elems']) out += "<br>" + data['timetable_elems'][elem];
-                result.html(out);
-            });
-        } else {
-            result.html('');
-        }
-    });
-
-    $('.search')
+    $('.ui.search')
         .search({
             // change search endpoint to a custom endpoint by manipulating apiSettings
             apiSettings: {
@@ -27,11 +9,3 @@ $(document).ready(function () {
         })
     ;
 });
-
-async function search(query) {
-    let ret = undefined;
-    await $.get("/search/?q=" + query, function (data) {
-        ret = data;
-    });
-    return ret;
-}
