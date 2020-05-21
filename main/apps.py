8c1b576e664269fbe31d055d2cef4285f1289076
Neisvestney <neisvestney113@gmail.com>
2020-05-21 21:59:23 +0300

    Auto creating study sessions

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,13 @@
+import sys
+
 from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    def ready(self):
+        if 'runserver' not in sys.argv:  # TODO возможно не запуститься через wsgi
+            return True
+        from main import tasks
+        tasks.session_creator()
