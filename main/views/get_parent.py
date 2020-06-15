af7796d3a7289d2d6939b57536976d8ad31cc193
Xefise <Xefise@gmail.com>
2020-06-15 20:31:48 +0300

    Add setModel

--- main/views/get_parent.py
+++ main/views/get_parent.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from main.models import *
+from main.models import Parent
+from main.views.functionsAndClasses.setModel import setModel
 
 
 @login_required(login_url="/login/")
 def get_parent(request, parent):
     parent = Parent.objects.get(id=parent)
+    setModel(parent, request)
     return render(request, 'parent.html', {'parent': parent})
--- main/views/get_session.py
+++ main/views/get_session.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 from main.models import StudySession
+from main.views.functionsAndClasses.setModel import setModel
 
 
 def get_session(request, session):
     session = StudySession.objects.get(id=session)
+    setModel(session, request)
     return render(request, 'study_session.html', {'session': session})
