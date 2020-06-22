b883e2e02210a5c4983671214babab48ec896afd
Neisvestney <30620564+Neisvestney@users.noreply.github.com>
2020-06-22 19:25:20 +0300

    Fix session creator (#4)
    * Fix threading
    
    * Fix session_creator

--- main/tasks.py
+++ main/tasks.py
@@ -1,4 +1,5 @@
 import threading
+import time
 from datetime import datetime
 import pytz
 from django.utils.timezone import now
@@ -6,10 +7,11 @@ import locale
 from DKControl import settings
 from main.models import StudySession, Group, Attending
 
-WAIT_SECONDS = 60
+WAIT_SECONDS = 1
 
 user_timezone = pytz.timezone(settings.TIME_ZONE)
 now().astimezone(user_timezone)
+locale.setlocale(locale.LC_ALL, 'ru_RU.utf8')
 
 
 def session_creator():
@@ -19,10 +21,10 @@ def session_creator():
                 if timetable_elem.beginTime >= datetime.now().time():
                     continue
                 if not [None for session in group.studysession_set.all()
-                        if session.date.strftime("%a").upper() == timetable_elem.day and session.date.date() == now().date() and session.date.time() > datetime.now().time()]:
+                        if session.date.strftime("%a").upper() == timetable_elem.day and session.date.date() == now().date() and session.date.time() == timetable_elem.beginTime]:
 
                     datetime_begin = now()
-                    datetime_begin = datetime_begin.replace(hour=timetable_elem.beginTime.hour, minute=timetable_elem.beginTime.minute, second=0)
+                    datetime_begin = datetime_begin.replace(hour=timetable_elem.beginTime.hour, minute=timetable_elem.beginTime.minute, second=0, microsecond=0)
                     new_session = StudySession(date=datetime_begin, group=group)
                     new_session.save()
                     for student in group.students.all():
@@ -31,4 +33,6 @@ def session_creator():
 
                     print(f"[{datetime.now()}] New session {new_session}")
 
-    threading.Timer(WAIT_SECONDS, session_creator).start()
+    thread = threading.Timer(WAIT_SECONDS, session_creator)
+    thread.daemon = True
+    thread.start()
